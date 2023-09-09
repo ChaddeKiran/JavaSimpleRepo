f75ab11686c5f8aea80d6021a7404954bde083e5
Kiran Chadde <87512557+ChaddeKiran@users.noreply.github.com>
2023-09-09 20:45:17 +0530

    uploaded simple java file
    Sample java file which prints Hello Kiran

---
+++ Sample.java
@@ -0,0 +1,12 @@
+public class Sample {
+    public static void main(String... args) {
+       
+       System.out.println("Hello Kiran");
+        // System.out.println("args : " + args.length);
+
+        //int a[7]={1,2,3,4};
+        //a={5,6,7,8,9};
+        //a[4]=100;
+        //a[5]=20
+    }
+}
